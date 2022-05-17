40f363585f008745bac85e89719ad2263aa94713
shimtom <12668850+shimtom@users.noreply.github.com>
2022-05-18 00:23:28 +0900

    Change command usage

--- install.sh
+++ install.sh
@@ -1,180 +1,320 @@
 #!/usr/bin/env bash
 
-set -ue
+set -u
 
-DOTFILES="$(pwd)"
-COLOR_GRAY="\033[1;38;5;243m"
-COLOR_BLUE="\033[1;34m"
-COLOR_GREEN="\033[1;32m"
-COLOR_RED="\033[1;31m"
-COLOR_PURPLE="\033[1;35m"
-COLOR_YELLOW="\033[1;33m"
-COLOR_NONE="\033[0m"
+declare MINIMUM=false
+declare BACKUP=false
+declare FORCE=false
+declare INTERACTIVE=false
 
-title() {
-    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
-    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+declare n=0
+declare BACKUP_DIR=$HOME/dotfiles.backup
+while [ -e "$BACKUP_DIR" ]; do
+    BACKUP_DIR="${HOME}/dotfiles.backup-$((++n))"
+done
+declare -r DOTFILES="$(pwd)"
+
+declare -r COLOR_GRAY="\033[1;38;5;243m"
+declare -r COLOR_BLUE="\033[1;34m"
+declare -r COLOR_GREEN="\033[1;32m"
+declare -r COLOR_RED="\033[1;31m"
+declare -r COLOR_PURPLE="\033[1;35m"
+declare -r COLOR_YELLOW="\033[1;33m"
+declare -r COLOR_NONE="\033[0m"
+
+show_spinner() {
+    local -r frames='/-\|'
+    local -r n=${#frames}
+
+    local -r pid="$1"
+    local -r cmds="$2"
+    local -r msg="$3"
+
+    local i=0
+    local frame_text=""
+
+    # Provide more space so that the text hopefully
+    # doesn't reach the bottom line of the terminal window.
+    #
+    # This is a workaround for escape sequences not tracking
+    # the buffer position (accounting for scrolling).
+    #
+    # See also: https://unix.stackexchange.com/a/278888
+    echo -e "\n\n"
+    tput cuu 3
+    tput sc
+
+    # Display spinner while the commands are being executed.
+    while kill -0 "$pid" &> /dev/null; do
+        frame_text="[${frames:i++%n:1}] $msg"
+
+        # print frame text.
+        echo -n "$frame_text"
+        sleep 0.2
+
+        # clear frame text.
+        echo -ne "\r"
+    done
+
 }
 
-error() {
-    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
-    exit 1
+print_title() {
+    echo -e "${COLOR_PURPLE}$1${COLOR_NONE}"
 }
 
-warning() {
-    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+print_warning() {
+    echo -e "[${COLOR_YELLOW}!${COLOR_NONE}] $1"
 }
 
-info() {
-    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+print_skip() {
+    echo -e "[ ] $1  ${COLOR_YELLOW}... skipped${COLOR_NONE}"
 }
 
-success() {
-    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+print_error() {
+    echo -e "[${COLOR_RED}✖${COLOR_NONE}] $1" 1>&2
+}
+
+error_stream() {
+    while read -r line; do
+        echo -e "↳ ${COLOR_RED}ERROR: $line${COLOR_NONE}" 1>&2
+    done
+}
+
+print_result() {
+    if [ "$1" -eq 0 ]; then
+        # success
+        echo -e "[${COLOR_GREEN}✔${COLOR_NONE}] $2"
+    elif [ "$1" -eq 1 ]; then
+        print_error $2
+    else
+        print_skip $2
+    fi
+
+    return "$1"
+}
+
+ask() {
+    echo -e -n "[${COLOR_YELLOW}?${COLOR_NONE}] $1: "
+    read -r
+}
+
+ask_for_confirmation() {
+    echo -e -n "[${COLOR_YELLOW}?${COLOR_NONE}] $1 (y/n): "
+    read -r
+}
+
+kill_all_subprocesses() {
+    local pid=""
+    jobs -p
+
+    for pid in $(jobs -p); do
+        kill "$pid"
+        wait "$pid" &> /dev/null
+    done
+}
+
+execute() {
+    local -r cmd="$1"
+    local -r msg="${2:-$1}"
+    local -r tmp_file="$(mktemp /tmp/dotfiles.XXXXX)"
+
+    local exit_code=0
+    local cmd_pid=""
+
+    # If the current process is ended,
+    # also end all its subprocesses.
+    trap -p "EXIT" | grep "kill_all_subprocesses" &> /dev/null \
+        || trap 'kill_all_subprocesses' "EXIT"
+
+    # Execute commands in background
+    # shellcheck disable=SC2261
+    # eval "$cmd" &> /dev/null 2> "$tmp_file" &
+    eval "$cmd" \
+        &> /dev/null \
+        2> "$tmp_file" &
+    cmd_pid=$!
+
+    # Show a spinner if the commands
+    # require more time to complete.
+    show_spinner "$cmd_pid" "$cmd" "$msg"
+
+    # Wait for the commands to no longer be executing
+    # in the background, and then get their exit code.
+    wait "$cmd_pid" &> /dev/null
+    exit_code=$?
+
+    # Print output based on what happened.
+    print_result $exit_code "$msg"
+
+    if [ $exit_code -ne 0 ]; then
+        error_stream < "$tmp_file"
+    fi
+
+    rm -rf "$tmp_file"
+
+    return $exit_code
+}
+
+unique_name() {
+    local -r original_name="$1"
+    local n=0
+    local name="$original_name"
+
+    while [ -e "$name" ]; do
+        name="$original_name-$((++n))"
+    done
+    echo "$name"
+}
+
+link() {
+    local -r source="$1"
+    local -r target="$2"
+
+    if "$FORCE"; then
+        execute 'ln -sf "$source" "$target"' "link '$file' → '$target'"
+    else
+        if [ -e "$target" ]; then
+            if "$BACKUP"; then
+                backup_target=$(unique_name $BACKUP_DIR/$(basename $target | sed -e "s/^\.//"))
+                execute 'mv "$target" "$backup_target"' "backup '$target' → '$backup_target'"
+            elif "$INTERACTIVE"; then
+                ask_for_confirmation "'$target' already exists, do you want to overwrite it?"
+                if [[ $REPLY =~ ^([Yy])$ ]]; then
+                    execute 'ln -sf "$source" "$target"' "link '$file' → '$target'"
+                else
+                    print_skip "link '$file' → '$target'"
+                fi
+            else
+                print_error "link '$file' → '$target'"
+                echo -e "↳ ${COLOR_RED}ERROR: '$target' already exists.${COLOR_NONE}" 1>&2
+            fi
+        else
+            execute 'ln -s "$source" "$target"' "link '$file' → '$target'"
+        fi
+    fi
+
+    return 0
 }
 
 setup_sh() {
-    title "Setting up Sh"
+    local -r file="$DOTFILES/sh/profile"
+    local -r target="$HOME/.$(basename "$file")"
 
-    file="$DOTFILES/sh/profile"
-    target="$HOME/.$(basename "$file")"
-    info "Creating symlink for $file"
-    ln -sf "$file" "$target"
+    print_title "Shell"
+    link "$file" "$target"
 }
 
 setup_bash() {
-    title "Setting up Bash"
+    print_title "Bash"
 
     for file in "$DOTFILES/bash/bashrc" "$DOTFILES/bash/bash_logout"; do
         target="$HOME/.$(basename "$file")"
-        info "Creating symlink for $file"
-        ln -sf "$file" "$target"
+
+        link "$file" "$target"
     done
 }
 
 setup_zsh() {
-    title "Setting up Zsh"
+    local -r file="$DOTFILES/zsh/zshrc"
+    local -r target="$HOME/.$(basename "$file")"
 
-    file="$DOTFILES/zsh/zshrc"
-    target="$HOME/.$(basename "$file")"
-    info "Creating symlink for $file"
-    ln -sf "$file" "$target"
+    print_title "Zsh"
+    link "$file" "$target"
 }
 
 setup_config() {
-    title "Setting up config"
+    print_title "\$XDG_CONFIG_HOME/config"
 
     XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
-    info "Installing to $XDG_CONFIG_HOME"
     if [ ! -d "$XDG_CONFIG_HOME" ]; then
-        info "Creating $XDG_CONFIG_HOME"
-        mkdir -p "$XDG_CONFIG_HOME"
+        execute 'mkdir -p "$XDG_CONFIG_HOME"' "make \$XDG_CONFIG_HOME directory: '$XDG_CONFIG_HOME'"
     fi
 
-    while IFS= read -r -d '' config; do
+    for config in `find -H "$DOTFILES/config" -mindepth 1 -maxdepth 1`; do
         target="$XDG_CONFIG_HOME/$(basename "$config")"
-        info "Creating symlink for $config"
-        rm -rf "$target"
-        ln -sf "$config" "$target"
-    done <   <(find -H "$DOTFILES/config" -mindepth 1 -maxdepth 1 -print0)
+        link "$config" "$target"
+    done
+
 }
 
 setup_git() {
-    title "Setting up Git"
+    local -r default_name=$(git config user.name)
+    local -r default_email=$(git config user.email)
 
-    defaultName=$(git config user.name)
-    defaultEmail=$(git config user.email)
+    print_title "Git"
 
-    read -rp "Name [$defaultName]: " name
-    read -rp "Email [$defaultEmail]: " email
+    ask "Enter git user name [$default_name]"
+    name="${REPLY:-$default_name}"
+    ask "Enter git user email [$default_email]"
+    email="${REPLY:-$default_email}"
 
-    git config -f ~/.gitconfig_local user.name "${name:-$defaultName}"
-    git config -f ~/.gitconfig_local user.email "${email:-$defaultEmail}"
+    execute 'git config -f ~/.gitconfig.local user.name "$name"' "set git user name: '$name'"
+    execute 'git config -f ~/.gitconfig.local user.email "$email"' "set git user email: '$name'"
 
-    if [[ "$(uname)" == "Darwin" ]]; then
-        git config -f ~/.gitconfig_local credential.helper "osxkeychain"
+    if [ "$(uname -s)" == "Darwin" ]; then
+        execute 'git config -f ~/.gitconfig.local credential.helper "osxkeychain"' "set git credential helper: 'osxkeychain'"
     else
-        read -rn 1 -p "Save user and password to an unencrypted file to avoid writing? [y/N]: " save
-        if [[ $save =~ ^([Yy])$ ]]; then
-            git config -f ~/.gitconfig_local credential.helper "store"
+        ask_for_confirmation "Save user and password to an unencrypted file to avoid writing?"
+        if [[ $REPLY =~ ^([Yy])$ ]]; then
+            execute 'git config -f ~/.gitconfig.local credential.helper "store"' "set git credential helper: 'store'"
         else
-            git config -f ~/.gitconfig_local credential.helper "cache --timeout 3600"
+            execute 'git config -f ~/.gitconfig.local credential.helper "cache --timeout 3600"' "set git credential helper: 'cache --timeout 3600'"
         fi
     fi
 }
 
-backup() {
-    BACKUP_DIR=$HOME/dotfiles-backup
 
-    echo "Creating backup directory at $BACKUP_DIR"
-    mkdir -p "$BACKUP_DIR" "$BACKUP_DIR/config"
+for opt in "$@"; do
+    case $opt in
+        --minimum)
+            MINIMUM=true
+            ;;
+        -b | --backup)
+            BACKUP=true
+            ;;
+        -f | --force)
+            FORCE=true
+            ;;
+        -i | --interactive)
+            INTERACTIVE=true
+            ;;
+        -h | --help)
+            echo "Usage:"
+            echo "  install.sh [OPTION]"
+            echo "  install.sh --minimum [OPTION]"
+            echo
+            echo "Install dotfiles."
+            echo
+            echo " --minimum          install only standard shell dotfiles"
+            echo " -b, --backup       make a backup of each existing destnation"
+            echo " -i, --interactive  prompt whether to remove destinations"
+            echo " -f, --force        remove existing destinations"
+            echo " -h, --help         display this help and exist"
+            exit 0
+            ;;
+        *)
+            echo "illegal option or syntax: '$opt'" 1>&2
+            exit 1
+    esac
+done
 
-    # backup sh, bash and zsh
-    for file in "$DOTFILES/sh/profile" "$DOTFILES/bash/bashrc" "$DOTFILES/bash/bash_logout" "$DOTFILES/zsh/zshrc"; do
-        filename=".$(basename "$file")"
-        target="$HOME/$filename"
-        if [ -f "$target" ]; then
-            echo "backing up $filename"
-            cp "$target" "$BACKUP_DIR"
-        else
-            warning "$filename does not exist at this location or is a symlink"
-        fi
-    done
+setup_sh
+# if "$BACKUP" &&  [ ! -e "$BACKUP_DIR" ]; then
+#     execute 'mkdir -p "$BACKUP_DIR"' "make backup directory: '$BACKUP_DIR'"
+# fi
 
-    # backup xdg config
-    XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
-
-    while IFS= read -r -d '' config; do
-        config=$(basename "$config")
-        target="$XDG_CONFIG_HOME/$config"
-
-        if [ -f "$target" ] || [ -d "$target" ]; then
-            echo "backing up $config"
-            cp -rf "$target" "$BACKUP_DIR/config"
-        else
-            warning "$config does not exist at this location or is a symlink"
-        fi
-    done <   <(find -H "./config" -mindepth 1 -maxdepth 1 -print0)
-
-}
-
-
-case "$1" in
-    backup)
-        backup
-        ;;
-    minimum)
-        setup_sh
-        setup_bash
-        ;;
-    link)
-        setup_sh
-        setup_bash
-        setup_zsh
-        setup_config
-        ;;
-    git)
-        setup_git
-        ;;
-    develop)
-        setup_sh
-        setup_config
-        setup_git
-        ;;
-    all)
-        backup
-        setup_sh
-        setup_bash
-        setup_zsh
-        setup_config
-        setup_git
-        ;;
-    *)
-        echo -e $"\nUsage: $(basename "$0") {backup|minimum|link|git|all}\n"
-        exit 1
-        ;;
-esac
-
-echo -e
-success "Done."
+# if "$MINIMUM"; then
+#     setup_sh
+#     if [ "$(uname -s)" == "Darwin" ]; then
+#         setup_zsh
+#     else
+#         setup_bash
+#     fi
+# else
+#     setup_sh
+#     setup_bash
+#     setup_zsh
+#     setup_config
+#     setup_git
+# fi
